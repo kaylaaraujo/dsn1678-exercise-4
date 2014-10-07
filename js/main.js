1850ad640ce57a7af28096b28def1cd8c6beb79c
kaylaaraujo <arau0014@algonquinlive.com>
2014-10-07 10:44:13 -0400

    added variables for 4 ships, added animation

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $shuttle = $('.shuttle');
+var $shuttleSection = $('.shuttle-section');
+var $shipsSection = $('.ships-section');
+var $ship1 = $('.ship-1');
+var $ship2 = $('.ship-2');
+var $ship3 = $('.ship-3');
+var $ship4 = $('.ship-4');
+
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -15,3 +23,16 @@ $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
 }, { offset: '50%' });
 	
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade-up');
+}, { offset: '50%' });
+
+$shipsSection.waypoint(function () {
+	$ship1.addClass('js-ships-1-3');
+	$ship3.addClass('js-ships-1-3');
+}, { offset: '50%' });
+
+$shipsSection.waypoint(function () {
+	$ship2.addClass('js-ships-2-4');
+	$ship4.addClass('js-ships-2-4');
+}, { offset: '50%' });
